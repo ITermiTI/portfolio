f24100a8d189feab198c8f9823ad97b2358b5b3e
ITermiTI <pawliko@poczta.onet.eu>
2020-06-23 22:04:05 +0200

    Updates

---
+++ precache-manifest.f14dd988a29d782ce7bd250816695619.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a84d9dd5224c859468ed2ab67ea7c30",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "74e6f22c3d42c460a111",
+    "url": "/portfolio/static/css/main.8c4bd31a.chunk.css"
+  },
+  {
+    "revision": "85d4841f22e63b934af7",
+    "url": "/portfolio/static/js/2.2cad98dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2cad98dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e6f22c3d42c460a111",
+    "url": "/portfolio/static/js/main.043a9cc3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2f83b05ef9b06e19d819ad96fffb18ca",
+    "url": "/portfolio/static/media/hockey.2f83b05e.jpg"
+  },
+  {
+    "revision": "2b36626b8f640f3a9ebe357c69d5f9a4",
+    "url": "/portfolio/static/media/me.2b36626b.jpg"
+  },
+  {
+    "revision": "deee2ffb90b7832139b4f6bab249345a",
+    "url": "/portfolio/static/media/politechnika-wroclawska.deee2ffb.png"
+  },
+  {
+    "revision": "e51f810bd0eb48722ec812e16805259d",
+    "url": "/portfolio/static/media/scubadiving.e51f810b.jpg"
+  },
+  {
+    "revision": "8c1d576d1f87517d1e85b2b40fd5ec66",
+    "url": "/portfolio/static/media/skydiving.8c1d576d.png"
+  },
+  {
+    "revision": "60a157ed1d1cec082334e92943f9c4e3",
+    "url": "/portfolio/static/media/travel.60a157ed.jpg"
+  },
+  {
+    "revision": "8b553b21de2457ffc71fb0c760369dd9",
+    "url": "/portfolio/static/media/welcome.8b553b21.jpg"
+  },
+  {
+    "revision": "17393114bf65fab46ac3b0a09df0bd10",
+    "url": "/portfolio/static/media/xeva.17393114.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04b2d1602bece12973d75a4c8668e4a9.js"
+  "/portfolio/precache-manifest.f14dd988a29d782ce7bd250816695619.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
